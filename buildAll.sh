e4eb6c1da7f5cb528390fdec43623453276659a2
subwiz <subwiz@localhost>
2006-02-02 17:40:54 +0000

    copying build artifacts to site folder removed

--- buildAll.sh
+++ buildAll.sh
@@ -10,8 +10,3 @@ mvn site
 
 cp src/main/resources/wizcryptmsg.properties target/site/
 
-cp target/WizCrypt-1.0-jar-with-dependencies.jar target/site/
-cp target/WizCrypt-1.0-src.tar.gz target/site/
-cp target/WizCrypt-1.0-src.tar.bz2 target/site/
-cp target/WizCrypt-1.0-src.zip target/site/
-
