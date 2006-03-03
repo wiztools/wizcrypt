27d59dbffe18d3bbfc53d877ba57eac024157bda
subwiz <subwiz@users.noreply.github.com>
2006-03-03 18:34:53 +0000

    localized messages which had been missed in the previous iterations

--- src/main/java/org/wiztools/crypt/Decrypt.java
+++ src/main/java/org/wiztools/crypt/Decrypt.java
@@ -19,41 +19,43 @@ import java.util.ResourceBundle;
 import java.util.Arrays;
 
 public class Decrypt implements IProcess{
-	private final String ALGO = ResourceBundle.getBundle("wizcrypt").getString("algorithm");
-
-	private Cipher cipher;
-	private byte[] passKeyHash;
-
-	public void init(String keyStr) throws NoSuchAlgorithmException, InvalidKeyException, NoSuchPaddingException{
-		SecretKey key = new SecretKeySpec(keyStr.getBytes(), ALGO);
-		cipher = Cipher.getInstance(ALGO);
-		cipher.init(Cipher.DECRYPT_MODE, key);
-		passKeyHash = PassHash.passHash(keyStr);
-	}
-
-	public void process(File file) throws IOException, FileNotFoundException, PasswordMismatchException{
-		String path = file.getCanonicalPath();
-		if(!path.endsWith(".wiz")){
-			throw new FileNotFoundException("File does not end with `.wiz`: "+path);
-		}
-		String newPath = path.replaceFirst(".wiz$", "");
-		File outFile = new File(newPath);
-		FileInputStream fis = new FileInputStream(file);
-		CipherOutputStream cos = new CipherOutputStream(
-					new FileOutputStream(outFile), cipher);
-		// read 16 bytes from fis
-		byte[] filePassKeyHash = new byte[16];
-		fis.read(filePassKeyHash, 0, 16);
-		if(!Arrays.equals(passKeyHash, filePassKeyHash)){
-			throw new PasswordMismatchException("Password does not match: "+path);
-		}
-
-		int i = -1;
-		while((i=fis.read()) != -1){
-			cos.write((char)i);
-		}
-		cos.close();
-		fis.close();
-		file.delete();
-	}
+    private final String ALGO = ResourceBundle.getBundle("wizcrypt").getString("algorithm");
+    
+    private static final ResourceBundle rb = ResourceBundle.getBundle("wizcryptmsg");
+    
+    private Cipher cipher;
+    private byte[] passKeyHash;
+    
+    public void init(String keyStr) throws NoSuchAlgorithmException, InvalidKeyException, NoSuchPaddingException{
+        SecretKey key = new SecretKeySpec(keyStr.getBytes(), ALGO);
+        cipher = Cipher.getInstance(ALGO);
+        cipher.init(Cipher.DECRYPT_MODE, key);
+        passKeyHash = PassHash.passHash(keyStr);
+    }
+    
+    public void process(File file) throws IOException, FileNotFoundException, PasswordMismatchException{
+        String path = file.getCanonicalPath();
+        if(!path.endsWith(".wiz")){
+            throw new FileNotFoundException(rb.getString("err.file.not.end.wiz")+path);
+        }
+        String newPath = path.replaceFirst(".wiz$", "");
+        File outFile = new File(newPath);
+        FileInputStream fis = new FileInputStream(file);
+        CipherOutputStream cos = new CipherOutputStream(
+                new FileOutputStream(outFile), cipher);
+        // read 16 bytes from fis
+        byte[] filePassKeyHash = new byte[16];
+        fis.read(filePassKeyHash, 0, 16);
+        if(!Arrays.equals(passKeyHash, filePassKeyHash)){
+            throw new PasswordMismatchException(rb.getString("err.pwd.not.match")+path);
+        }
+        
+        int i = -1;
+        while((i=fis.read()) != -1){
+            cos.write((char)i);
+        }
+        cos.close();
+        fis.close();
+        file.delete();
+    }
 }
