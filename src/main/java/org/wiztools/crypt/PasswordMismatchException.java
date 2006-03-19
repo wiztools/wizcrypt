49d7644e6f94bd8bd14851197f2b2256b6f54ccd
subwiz <subwiz@localhost>
2006-03-19 18:22:03 +0000

    removed duplicate functionality available in Exception class

--- src/main/java/org/wiztools/crypt/PasswordMismatchException.java
+++ src/main/java/org/wiztools/crypt/PasswordMismatchException.java
@@ -2,15 +2,8 @@ package org.wiztools.crypt;
 
 public class PasswordMismatchException extends Exception{
 
-	private String msg;
-
 	public PasswordMismatchException(String msg){
 		super(msg);
-		this.msg = msg;
-	}
-
-	public String getMessage(){
-		return this.msg;
 	}
 
 }
