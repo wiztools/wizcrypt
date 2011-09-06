e069e2595ee27770d9ef971974827d39a607203b
Subhash Chandran <subwiz@users.noreply.github.com>
2011-09-06 23:21:18 +0530

    Placeholder implementation classes for supporting legacy format.

---
+++ src/main/java/org/wiztools/wizcrypt/DecryptLegacy.java
@@ -0,0 +1,17 @@
+package org.wiztools.wizcrypt;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ *
+ * @author subhash
+ */
+public class DecryptLegacy extends AbstractWizCrypt {
+
+    @Override
+    public void process(File file, File outFile, char[] password, ParamBean cpb) throws IOException, WizCryptException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
